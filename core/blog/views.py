248886adebe5be73c9f5104b144e7000acdc4f95
Alireza <ad665529@gmail.com>
2026-08-03 07:33:34 +0330

    fix(blog): fix blog urls include typo and permission_classes attribute in PostViewSet

--- core/blog/views.py
+++ core/blog/views.py
@@ -5,7 +5,7 @@ from .serializers import PostSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class  = PostSerializer
-    permissions_class = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
     def perform_create(self , serializer):
         serializer.save(author=self.request.user)
--- core/core/urls.py
+++ core/core/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/accounts/" , include('accounts.urls')),
-    path("api/blog/" , include('blog.urld')),
+    path("api/blog/" , include('blog.urls')),
     
 ]
 
