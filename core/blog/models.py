27f96357073c5a721b2aa39882ab50f04dd8c3f2
Alireza <ad665529@gmail.com>
2026-08-22 15:07:44 +0330

    feat(blog): enhance Post model with Tag relation and auto-slug,

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,23 +1,33 @@
 from django.db import models
+from django.utils.text import slugify
 from django.conf import settings
 
 # Create your models here.
 
 class Post(models.Model):
     """
-    This is a class to define Post model for blog app
+    This is a class to define Post model for blog app 
     """
+    
+    slug = models.SlugField(max_length=256 , unique=True , blank=True , allow_unicode=True)
+    
     title = models.CharField(max_length=256)
     content = models.TextField(blank=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.CASCADE)
     image = models.ImageField(null=True , blank=True)
     category = models.ForeignKey("Category" , on_delete=models.SET_NULL , null = True)
-    publish= models.BooleanField()
+    tags = models.ManyToManyField("Tag" ,  blank=True)
+    is_published = models.BooleanField(default=False)
     
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(null=True , blank=True)
     
+    def save(self , *args , **kwargs ):
+        if not self.slug:
+            self.slug = slugify(self.title , allow_unicode=True)
+            
+        super.save(*args , **kwargs)
     
     def __str__(self):
         return self.title
@@ -26,6 +36,17 @@ class Post(models.Model):
 class Category(models.Model):
     """
     This is a class to define Category model for blog app
+    """
+    
+    name = models.CharField(max_length=256)
+    
+    
+    def __str__(self):
+        return self.name
+    
+class Tag(models.Model):
+    """
+    This is a class to define Tag model for blog app
     """
     
     name = models.CharField(max_length=256)
