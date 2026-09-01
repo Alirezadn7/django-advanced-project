fcacd7b62f74e68182043d3e64f243a41a161e18
Alireza <ad665529@gmail.com>
2026-09-01 22:54:37 +0330

    fix(settings): correct typo django_filetrs -> django_filters in INSTALLED_APPS

--- core/core/settings/base.py
+++ core/core/settings/base.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     # 3rd party
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
-    "django_filetrs",
+    "django_filters",
     
     # Local
     "accounts",
